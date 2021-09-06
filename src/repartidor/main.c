fbf0952a0937b332958cfce33551e506d2814cc1
Sebastián Orrego <sforrego@uc.cl>
2021-09-06 12:28:03 -0300

    dccuber compilado final

--- src/repartidor/main.c
+++ src/repartidor/main.c
@@ -17,6 +17,7 @@ int tiempo_sem1 = -1;
 int tiempo_sem2 = -1;
 int tiempo_sem3 = -1;
 int tiempo_bodega = -1;
+int avanzar = 0;
 char archivo[32];
 
 void handle_sigabrt_repartidor(int sig)
@@ -24,49 +25,14 @@ void handle_sigabrt_repartidor(int sig)
   FILE *f = fopen(archivo, "w");
   fprintf(f, "%d,%d,%d,%d\n", tiempo_sem1, tiempo_sem2, tiempo_sem3, tiempo_bodega);
   fclose(f);
+  exit(0);
 }
 
 void handle_sigalrm_repartidor(int sig)
 {
-  tiempo++; // paso 1 segundo, se aumento el tiempo
-
-  printf("Repartidor %d INSIDE ALARM\n",my_pid); // reviso si esta entrando a la alarm, pq ahora no lo hace
-      
-  if (next_semaforo == 3 && current_pos+1 == pos_semaforos[next_semaforo]){ // reviso si llegó a la bodega
-  current_pos++;
-  tiempo_bodega = tiempo;
-  printf("Repartidor %d llega a la bodega\n",my_pid);
-  FILE *f = fopen(archivo, "w");
-  fprintf(f, "%d,%d,%d,%d\n", tiempo_sem1, tiempo_sem2, tiempo_sem3, tiempo_bodega);  
-  fclose(f);
-    } 
-    else 
-    {
-      if (current_pos+1==pos_semaforos[next_semaforo]){ // reviso si viene un semaforo
-      if(next_semaforo < 3 && state_semaforos[next_semaforo] == 1){ // reviso que el semaforo esté en verde 
-        printf("Repartidor %d se encuentra con un semaforo en verde, currentpos: %d\n",my_pid,current_pos);
-        switch(next_semaforo){ // registro cuando tiempo se demoro en llegar al semaforo
-          case 0:
-          tiempo_sem1 = tiempo;
-          break;
-          case 1:
-          tiempo_sem2 = tiempo;
-          break;
-          case 2:
-          tiempo_sem3 = tiempo;
-          break;
-                 }
-        current_pos++;
-        next_semaforo++;
-      }else{
-        printf("Repartidor %d espera en un semaforo en ROJO, current pos: %d\n",my_pid,current_pos);
-      }
-    } else {
-      current_pos++;
-    }
-    alarm(1);}
-    
+  avanzar = 1;
 }
+
 void handle_sigusr1(int sig, siginfo_t *siginfo, void *context)
 {
   int number_received = siginfo->si_value.sival_int;
@@ -78,6 +44,10 @@ void handle_sigusr1(int sig, siginfo_t *siginfo, void *context)
       state_semaforos[number_received] = 0;
       break;
   }
+}
+void handle_sigint(int sig)
+{
+  
 }
 
 
@@ -96,23 +66,50 @@ int main(int argc, char const *argv[])
   state_semaforos[2] = atoi(argv[6]);
   indice = atoi(argv[7]);
   snprintf(archivo, sizeof archivo, "repartidor_%d.txt", indice);
-  printf("I'm the REPARTIDOR process and my PID is: %i\n", my_pid);
-  printf("%d %d %d\n",state_semaforos[0],state_semaforos[1],state_semaforos[2]);
+  printf("REPARTIDOR %i creado\n", my_pid);
   // Conectamos SIGALRM a handle_sigalrm
   signal(SIGALRM, handle_sigalrm_repartidor);
   signal(SIGABRT, handle_sigabrt_repartidor);
+  signal(SIGINT, handle_sigint);
   connect_sigaction(SIGUSR1, handle_sigusr1);
 
   alarm(1); 
-  printf("ALARM has been set\n");
-  sleep(2);// mandar sigalrm en 1 segundo 
   while(true){
-
+    if(avanzar){
+      tiempo++; // paso 1 segundo, se aumento el tiempo
+      
+      if (next_semaforo == 3 && current_pos+1 == pos_semaforos[next_semaforo]){ // reviso si llegó a la bodega
+        current_pos++;
+        tiempo_bodega = tiempo;
+        printf("Repartidor %d llega a la bodega\n",my_pid);
+        break;
+      } else { // si no ha llegado a la bodega avanzar
+        if (current_pos+1==pos_semaforos[next_semaforo]){ // reviso si viene un semaforo
+          if(next_semaforo < 3 && state_semaforos[next_semaforo] == 1){ // reviso que el semaforo esté en verde 
+            printf("Repartidor %d se encuentra con un semaforo en verde, currentpos: %d\n",my_pid,current_pos);
+              switch(next_semaforo){ // registro cuanto tiempo se demoro en llegar al semaforo
+                case 0:
+                tiempo_sem1 = tiempo;
+                break;
+                case 1:
+                tiempo_sem2 = tiempo;
+                break;
+                case 2:
+                tiempo_sem3 = tiempo;
+                break;
+                }
+            current_pos++;
+            next_semaforo++;
+      } else { // el semaforo esta en rojo
+        printf("Repartidor %d espera en un semaforo en ROJO, current pos: %d\n",my_pid,current_pos);
+      }
+    } else { // no hay semaforo
+        current_pos++;
     }
-  
+      avanzar = 0;
+      alarm(1);}
+    }
+  }
 
-
-  printf("WHILE IS DONE\n");
-  kill(my_pid, SIGINT);
-
+  kill(my_pid,SIGABRT);
 }
