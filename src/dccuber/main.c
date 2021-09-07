560e427986ce9150bfc0b9d4f10feaa3b78249a1
unknown <jpdominguez@uc.cl>
2021-09-07 15:59:54 -0300

    Fix ctrl+c

--- src/dccuber/main.c
+++ src/dccuber/main.c
@@ -13,8 +13,8 @@
 int other_id;
 char **state_semaforos;
 int pid_semaforos[3] = {0, 0, 0}; // id del proceso de cada semaforo
-char *time_semaforos[3]; // tiempo que dura cada semaforo antes de cambiar 
-char *info_semaforos[8]; // 4 posiciones + 3 estados + el indice del repartidor
+char *time_semaforos[3];          // tiempo que dura cada semaforo antes de cambiar
+char *info_semaforos[8];          // 4 posiciones + 3 estados + el indice del repartidor
 char *arg_repartidor = "./repartidor";
 char *arg_semaforo = "./semaforo";
 char *semaforo_argv[3];
@@ -22,6 +22,7 @@ int repartidores_creados = 0;
 int total_repartidores;
 int time_repartidores;
 int pid_repartidores[10000];
+int more_repartidores = 1;
 // estado de los semaforos, 1 es verde
 int status = 0;
 pid_t wpid;
@@ -29,100 +30,121 @@ int fabrica_id;
 int crear_repartidor = 1;
 
 // propaga el abrt a los repartidores
-void handle_sigabrt_fabrica(){
-  for(int j=0;j<repartidores_creados;j++){
-    kill(pid_repartidores[j],SIGABRT);
+void handle_sigabrt_fabrica()
+{
+  more_repartidores = 0;
+  for (int j = 0; j < repartidores_creados; j++)
+  {
+    kill(pid_repartidores[j], SIGABRT);
   }
-  }
+}
 
 // Aca recibe los cambios de semaforos
 void handle_sigusr1(int sig, siginfo_t *siginfo, void *context)
 {
   int number_received = siginfo->si_value.sival_int;
-  if(strcmp(state_semaforos[number_received], "0") == 0){
-      state_semaforos[number_received] = "1";}
-  else{
-      state_semaforos[number_received] = "0";
+  if (strcmp(state_semaforos[number_received], "0") == 0)
+  {
+    state_semaforos[number_received] = "1";
+  }
+  else
+  {
+    state_semaforos[number_received] = "0";
   }
 
-  for(int j=0;j<repartidores_creados;j++){
-  send_signal_with_int(pid_repartidores[j],number_received);
+  for (int j = 0; j < repartidores_creados; j++)
+  {
+    send_signal_with_int(pid_repartidores[j], number_received);
   }
 }
 
-
-void handle_sigalrm(int sig){
+void handle_sigalrm(int sig)
+{
   crear_repartidor = 1;
-  }
+}
 
-void handle_sigint(int sig){
+void handle_sigint(int sig)
+{
   kill(fabrica_id, SIGABRT);
-  for(int i =0;i<3;i++){
-    kill(pid_semaforos[i],SIGABRT); // termino los semaforos
+  for (int i = 0; i < 3; i++)
+  {
+    kill(pid_semaforos[i], SIGABRT); // termino los semaforos
   }
-  for( int i = 0; i < 3; i++){
+  for (int i = 0; i < 3; i++)
+  {
     free(state_semaforos[i]);
   }
-  free(state_semaforos); 
+  free(state_semaforos);
+  exit(0);
 }
 
-void fabrica(){ 
+void handle_sigint_fabrica(int sig)
+{
+}
+
+void fabrica()
+{
 
   // Conectamos SIGALRM y SIGABRT a sus handlers
   signal(SIGALRM, handle_sigalrm);
-  signal(SIGABRT,handle_sigabrt_fabrica);
+  signal(SIGABRT, handle_sigabrt_fabrica);
+  signal(SIGINT, handle_sigint_fabrica);
   connect_sigaction(SIGUSR1, handle_sigusr1);
 
-  while(true){
-    if (crear_repartidor){
-        other_id = fork();
-        if(!other_id){
-            info_semaforos[4] = state_semaforos[0];
-            info_semaforos[5] = state_semaforos[1];
-            info_semaforos[6] = state_semaforos[2];
-            char indice[50];
-            sprintf(indice,"%d",repartidores_creados);
-            info_semaforos[7] = indice;
-            execvp(arg_repartidor,info_semaforos);
+  while (true)
+  {
+    if (crear_repartidor && more_repartidores)
+    {
+      other_id = fork();
+      if (!other_id)
+      {
+        info_semaforos[4] = state_semaforos[0];
+        info_semaforos[5] = state_semaforos[1];
+        info_semaforos[6] = state_semaforos[2];
+        char indice[50];
+        sprintf(indice, "%d", repartidores_creados);
+        info_semaforos[7] = indice;
+        execvp(arg_repartidor, info_semaforos);
+      }
+      else
+      {
+        pid_repartidores[repartidores_creados] = other_id;
+        repartidores_creados++;
+        if (repartidores_creados < total_repartidores)
+        {
+          alarm(time_repartidores);
         }
-        else{
-            pid_repartidores[repartidores_creados] = other_id;
-            repartidores_creados++;
-            if (repartidores_creados<total_repartidores){ 
-            alarm(time_repartidores);
-            } else {
-            break;
-                }
-            }
-        crear_repartidor = 0;
+        else
+        {
+          break;
         }
+      }
+      crear_repartidor = 0;
     }
-
+  }
 
   // wait for children to finish
   pid_t wpid;
   int status = 0;
-  while ((wpid = wait(&status)) > 0);
-
-
-  }
-
+  while ((wpid = wait(&status)) > 0)
+    ;
+}
 
 int main(int argc, char const *argv[])
 {
-  state_semaforos = malloc(3 * sizeof(char*));
-  for( int i = 0; i < 3; i++){
-    state_semaforos[i] = malloc(sizeof(char));
+  state_semaforos = malloc(3 * sizeof(char *));
+  for (int i = 0; i < 3; i++)
+  {
+    state_semaforos[i] = malloc(sizeof(char) + 1);
     strcpy(state_semaforos[i], "1");
-
   }
-  printf("Semaforos: %s - %s - %s\n",state_semaforos[0], state_semaforos[1], state_semaforos[2]);
+  printf("Semaforos: %s - %s - %s\n", state_semaforos[0], state_semaforos[1], state_semaforos[2]);
   printf("I'm the DCCUBER process and my PID is: %i\n", getpid());
   char *filename = argv[1];
   InputFile *data_in = read_file(filename);
 
   printf("Leyendo el archivo %s...\n", filename);
-  
+
   printf("- Lineas en archivo: %i\n", data_in->len);
   printf("- Contenido del archivo:\n");
 
@@ -140,65 +162,73 @@ int main(int argc, char const *argv[])
   }
   printf("\n");
 
+  // work
+  // char *state_semaforos = {'1','1','1'};
 
- // work
-// char *state_semaforos = {'1','1','1'}; 
+  info_semaforos[0] = data_in->lines[0][0];
+  info_semaforos[1] = data_in->lines[0][1];
+  info_semaforos[2] = data_in->lines[0][2];
+  info_semaforos[3] = data_in->lines[0][3];
 
-info_semaforos[0] = data_in->lines[0][0];
-info_semaforos[1] = data_in->lines[0][1];
-info_semaforos[2] = data_in->lines[0][2];
-info_semaforos[3] = data_in->lines[0][3];
+  time_semaforos[0] = data_in->lines[1][2];
+  time_semaforos[1] = data_in->lines[1][3];
+  time_semaforos[2] = data_in->lines[1][4];
 
-time_semaforos[0] = data_in -> lines[1][2];
-time_semaforos[1] = data_in -> lines[1][3];
-time_semaforos[2] = data_in -> lines[1][4];
+  total_repartidores = atoi(data_in->lines[1][1]);
+  time_repartidores = atoi(data_in->lines[1][0]);
 
-total_repartidores = atoi(data_in -> lines[1][1]);
-time_repartidores = atoi(data_in->lines[1][0]);
+  // semaforos
 
-// semaforos
-
- fabrica_id = fork();
- if(!fabrica_id){
- // fabrica
- fabrica();
- } else {
-  
-  signal(SIGINT, handle_sigint);
-
-  // creamos los 3 semaforos
-  for(int i=0;i<3;i++){
-  other_id = fork();
-    if(!other_id){
-      char nsemaforo[50];
-      char parent_id[50];
-      sprintf(nsemaforo,"%d",i);
-      sprintf(parent_id, "%d", fabrica_id);
-      semaforo_argv[0] = time_semaforos[i];
-      semaforo_argv[1] = nsemaforo;
-      semaforo_argv[2] = parent_id;
-    execvp(arg_semaforo,semaforo_argv);
-    } else {
-      pid_semaforos[i] = other_id;
-    }}
-
-  // espero que la fabrica termine
-  waitpid(fabrica_id,NULL,0);
-  
-  for(int i =0;i<3;i++){
-    kill(pid_semaforos[i],SIGABRT); // termino los semaforos
-  } 
-
-  pid_t wpid;
-  int status = 0;
-  while ((wpid = wait(&status)) > 0);
-
-  printf("Liberando memoria...\n");
-  input_file_destroy(data_in);
-  for( int i = 0; i < 3; i++){
-    free(state_semaforos[i]);
+  fabrica_id = fork();
+  if (!fabrica_id)
+  {
+    // fabrica
+    fabrica();
   }
-  free(state_semaforos);
- }
- 
+  else
+  {
+
+    signal(SIGINT, handle_sigint);
+
+    // creamos los 3 semaforos
+    for (int i = 0; i < 3; i++)
+    {
+      other_id = fork();
+      if (!other_id)
+      {
+        char nsemaforo[50];
+        char parent_id[50];
+        sprintf(nsemaforo, "%d", i);
+        sprintf(parent_id, "%d", fabrica_id);
+        semaforo_argv[0] = time_semaforos[i];
+        semaforo_argv[1] = nsemaforo;
+        semaforo_argv[2] = parent_id;
+        execvp(arg_semaforo, semaforo_argv);
+      }
+      else
+      {
+        pid_semaforos[i] = other_id;
+      }
+    }
+
+    // espero que la fabrica termine
+    waitpid(fabrica_id, NULL, 0);
+    for (int i = 0; i < 3; i++)
+    {
+      free(state_semaforos[i]);
+    }
+    free(state_semaforos);
+    input_file_destroy(data_in);
+    for (int i = 0; i < 3; i++)
+    {
+      kill(pid_semaforos[i], SIGABRT); // termino los semaforos
+    }
+
+    pid_t wpid;
+    int status = 0;
+    while ((wpid = wait(&status)) > 0)
+      ;
+
+    printf("Liberando memoria...\n");
+  }
 }
--- src/repartidor/main.c
+++ src/repartidor/main.c
@@ -6,9 +6,9 @@
 #include <stdlib.h>
 #include <signal.h>
 
-int pos_semaforos[4] = {0,0,0,0};
-int state_semaforos[3] = {0,0,0};
-int current_pos=0;
+int pos_semaforos[4] = {0, 0, 0, 0};
+int state_semaforos[3] = {0, 0, 0};
+int current_pos = 0;
 int next_semaforo = 0;
 int my_pid;
 int indice;
@@ -36,25 +36,23 @@ void handle_sigalrm_repartidor(int sig)
 void handle_sigusr1(int sig, siginfo_t *siginfo, void *context)
 {
   int number_received = siginfo->si_value.sival_int;
-  switch(state_semaforos[number_received]){
-    case 0:
-      state_semaforos[number_received] = 1;
-      break;
-    case 1:
-      state_semaforos[number_received] = 0;
-      break;
+  switch (state_semaforos[number_received])
+  {
+  case 0:
+    state_semaforos[number_received] = 1;
+    break;
+  case 1:
+    state_semaforos[number_received] = 0;
+    break;
   }
 }
 void handle_sigint(int sig)
 {
-  
 }
-
 
 int main(int argc, char const *argv[])
 {
   my_pid = getpid();
-
 
   // obtenemos la posición de cada semaforo y bodega
   pos_semaforos[0] = atoi(argv[0]);
@@ -66,50 +64,63 @@ int main(int argc, char const *argv[])
   state_semaforos[2] = atoi(argv[6]);
   indice = atoi(argv[7]);
   snprintf(archivo, sizeof archivo, "repartidor_%d.txt", indice);
-  printf("REPARTIDOR %i creado\n", my_pid);
+  printf("REPARTIDOR %i creado, semaforos %d-%d-%d\n", my_pid, state_semaforos[0], state_semaforos[1], state_semaforos[2]);
   // Conectamos SIGALRM a handle_sigalrm
   signal(SIGALRM, handle_sigalrm_repartidor);
   signal(SIGABRT, handle_sigabrt_repartidor);
   signal(SIGINT, handle_sigint);
   connect_sigaction(SIGUSR1, handle_sigusr1);
 
-  alarm(1); 
-  while(true){
-    if(avanzar){
+  alarm(1);
+  while (true)
+  {
+    if (avanzar)
+    {
       tiempo++; // paso 1 segundo, se aumento el tiempo
-      
-      if (next_semaforo == 3 && current_pos+1 == pos_semaforos[next_semaforo]){ // reviso si llegó a la bodega
+
+      if (next_semaforo == 3 && current_pos + 1 == pos_semaforos[next_semaforo])
+      { // reviso si llegó a la bodega
         current_pos++;
         tiempo_bodega = tiempo;
-        printf("Repartidor %d llega a la bodega\n",my_pid);
+        printf("Repartidor %d llega a la bodega\n", my_pid);
         break;
-      } else { // si no ha llegado a la bodega avanzar
-        if (current_pos+1==pos_semaforos[next_semaforo]){ // reviso si viene un semaforo
-          if(next_semaforo < 3 && state_semaforos[next_semaforo] == 1){ // reviso que el semaforo esté en verde 
-            printf("Repartidor %d se encuentra con un semaforo en verde, currentpos: %d\n",my_pid,current_pos);
-              switch(next_semaforo){ // registro cuanto tiempo se demoro en llegar al semaforo
-                case 0:
-                tiempo_sem1 = tiempo;
-                break;
-                case 1:
-                tiempo_sem2 = tiempo;
-                break;
-                case 2:
-                tiempo_sem3 = tiempo;
-                break;
-                }
+      }
+      else
+      { // si no ha llegado a la bodega avanzar
+        if (current_pos + 1 == pos_semaforos[next_semaforo])
+        { // reviso si viene un semaforo
+          if (next_semaforo < 3 && state_semaforos[next_semaforo] == 1)
+          { // reviso que el semaforo esté en verde
+            printf("Repartidor %d se encuentra con un semaforo en verde, currentpos: %d\n", my_pid, current_pos);
+            switch (next_semaforo)
+            { // registro cuanto tiempo se demoro en llegar al semaforo
+            case 0:
+              tiempo_sem1 = tiempo;
+              break;
+            case 1:
+              tiempo_sem2 = tiempo;
+              break;
+            case 2:
+              tiempo_sem3 = tiempo;
+              break;
+            }
             current_pos++;
             next_semaforo++;
-      } else { // el semaforo esta en rojo
-        printf("Repartidor %d espera en un semaforo en ROJO, current pos: %d\n",my_pid,current_pos);
+          }
+          else
+          { // el semaforo esta en rojo
+            printf("Repartidor %d espera en un semaforo en ROJO, current pos: %d\n", my_pid, current_pos);
+          }
+        }
+        else
+        { // no hay semaforo
+          current_pos++;
+        }
+        avanzar = 0;
+        alarm(1);
       }
-    } else { // no hay semaforo
-        current_pos++;
-    }
-      avanzar = 0;
-      alarm(1);}
     }
   }
 
-  kill(my_pid,SIGABRT);
+  kill(my_pid, SIGABRT);
 }
